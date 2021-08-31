0ccf79c526b8b2b2a52f5a5cce432745572f3410
Leo Luna Baima <leobaima01@gmail.com>
2021-08-30 21:57:48 -0300

    Componente header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export function Header() {
+    return (
+        <>
+            <div className="header">
+                <h1 className="logo-title">Traction Test</h1>
+            </div>
+            <div className="line" />
+
+        </>
+    )
+}
+
